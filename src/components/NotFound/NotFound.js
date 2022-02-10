206f976bd3b40a1a914f8ba23807faedde60efab
Sohazur <mdsohazurislam@gmail.com>
2022-02-10 04:31:43 +0400

    Updated notfound page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="text-center">
+    <div className="text-center vh-100">
       <h1>This page doesn't exist.</h1>
     </div>
   );
